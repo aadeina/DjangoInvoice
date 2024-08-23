134152451cd548576d75fc3ede039a328708880f
aadeina <amarmed4500@gmail.com>
2024-08-23 01:19:23 -0700

    Add customer image upload functionality and dynamic phone label update

--- fact_app/views.py
+++ fact_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
-from .models import *
+from .models import Customer, Invoice
+from django.contrib import messages
 
 # Create your views here.
 
@@ -33,4 +34,31 @@ class AddCustomerView(View):
         return render(request, self.template_name)
 
     def post(self, request, *args, **kwargs):
+        data = {
+            'name': request.POST.get('name'),
+            'email': request.POST.get('email'),
+            'phone': request.POST.get('phone'),
+            'address': request.POST.get('address'),
+            'sex': request.POST.get('sex'),
+            'age': request.POST.get('age'),
+            'city': request.POST.get('city'),
+            'zip_code': request.POST.get('zip'),
+            'save_by': request.user
+        }
+        # Include the image file in the data dictionary
+        image = request.FILES.get('image')
+        if image:
+            data['image'] = image
+
+        try:
+            created = Customer.objects.create(**data)
+
+            if created:
+                messages.success(request, "Customer registered successfully.")
+            else:
+                messages.error(request, "Sorry, please try again. The sent data is corrupt.")
+        
+        except Exception as e:    
+            messages.error(request, f"Sorry, our system detected the following issue: {e}.")
+
         return render(request, self.template_name)
